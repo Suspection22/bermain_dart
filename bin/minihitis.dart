8f99f700529aba70995bdec16b22c81007bc2ae1
suspection22 <danielpapilaya01@gmail.com>
2023-09-15 12:51:10 +0700

    Pertama nih boss

---
+++ bin/minihitis.dart
@@ -0,0 +1,5 @@
+import 'package:minihitis/minihitis.dart' as minihitis;
+
+void main(List<String> arguments) {
+  print('Hello world: ${minihitis.calculate()}!');
+}
---
+++ test/minihitis_test.dart
@@ -0,0 +1,8 @@
+import 'package:minihitis/minihitis.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
